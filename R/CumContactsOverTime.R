4b92bea878dc5c6b36e43f4240664817a8ed85b7
karina-d-wallrafen <97552770+karina-d-wallrafen@users.noreply.github.com>
2022-07-19 18:13:52 -0400

    Updates to cumulative deg dist plots

--- R/CumContactsOverTime.R
+++ R/CumContactsOverTime.R
@@ -55,34 +55,58 @@ rm(list=setdiff(ls(), c("bCumContactsJan", "cCumContactsJan", "bCumContactsApr",
 
 
 # PLOT --------------------------------------------------------------------
+library(zoo)
+library(reshape2)
+cCumContactsApr$date <- 1:nrow(cCumContactsApr)
+cCumContactsApr <- rbind(cCumContactsApr, c(nrow(cCumContactsApr) + 1, cCumContactsApr[nrow(cCumContactsApr), 2]))
+cCumContactsJan$date <- 1:nrow(cCumContactsJan)
+cCumContactsJan <- rbind(cCumContactsJan, c(nrow(cCumContactsJan) + 1, cCumContactsJan[nrow(cCumContactsJan), 2]))
+cCumContacts <- merge(cCumContactsJan, cCumContactsApr, by = "date", all = TRUE)
+cCumContacts <- rename(cCumContacts, Jan = cCumContacts.x)
+cCumContacts <- rename(cCumContacts, Apr = cCumContacts.y)
+cCumContacts <- melt(cCumContacts, id='date')
+cCumContacts <- rename(cCumContacts, Month = variable)
 
-g1 <- ggplot(rbind(cCumContactsJan, data.frame(date = as.Date("2022-01-22"), cCumContacts = tail(cCumContactsJan$cCumContacts, 1))), aes(x = date, y = cCumContacts)) +
-  geom_step() + xlab("Date") + ylab("Avg. Cumulative Cell-Level Contacts") +
-  scale_x_date(expand = c(0, 0), limits = as.Date(c("2022-01-11","2022-01-22"))) +
+g1 <- ggplot() + geom_step(data = cCumContacts, aes(x = date, y = value, color = Month, group = Month, alpha = Month), size = 1) +
+  xlab("Day") + ylab("Avg. Cumulative Cell-Level Contacts") +
+  scale_color_manual(values=c("darkred", "cornflowerblue")) + scale_alpha_manual(values = c(1, 0.8)) +
+  scale_x_continuous(expand = c(0,0),
+                     limits = c(1, 12),
+                     breaks = c(1, 3, 5, 7, 9, 11),
+                     labels = c("1" = "Day 1", "3" = "Day 3",
+                                "5" = "Day 5",
+                                "7" = "Day 7", "9" = "Day 9",
+                                "11" = "Day 11")) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 4)) +
-  ggtitle("Cumulative Cell-Level Contacts (January)")
+  ggtitle("Cumulative Cell-Level Contacts")
 
-g2 <- ggplot(rbind(bCumContactsJan, data.frame(date = as.Date("2022-01-22"), bCumContacts = tail(bCumContactsJan$bCumContacts, 1))), aes(x = date, y = bCumContacts)) +
-  geom_step() + xlab("Date") + ylab("Avg. Cumulative Block-Level Contacts") +
-  scale_x_date(expand = c(0, 0), limits = as.Date(c("2022-01-11","2022-01-22"))) +
+bCumContactsApr$date <- 1:nrow(bCumContactsApr)
+bCumContactsApr <- rbind(bCumContactsApr, c(nrow(bCumContactsApr) + 1, bCumContactsApr[nrow(bCumContactsApr), 2]))
+bCumContactsJan$date <- 1:nrow(bCumContactsJan)
+bCumContactsJan <- rbind(bCumContactsJan, c(nrow(bCumContactsJan) + 1, bCumContactsJan[nrow(bCumContactsJan), 2]))
+bCumContacts <- merge(bCumContactsJan, bCumContactsApr, by = "date", all = TRUE)
+bCumContacts <- rename(bCumContacts, Jan = bCumContacts.x)
+bCumContacts <- rename(bCumContacts, Apr = bCumContacts.y)
+bCumContacts <- melt(bCumContacts, id='date')
+bCumContacts <- rename(bCumContacts, Month = variable)
+
+g2 <- ggplot() + geom_step(data = bCumContacts, aes(x = date, y = value, color = Month, group = Month, alpha = Month), size = 1) +
+  xlab("Day") + ylab("Avg. Cumulative Block-Level Contacts") +
+  scale_color_manual(values=c("darkred", "cornflowerblue")) + scale_alpha_manual(values = c(1, 0.8)) +
+  scale_x_continuous(expand = c(0,0),
+                     limits = c(1, 12),
+                     breaks = c(1, 3, 5, 7, 9, 11),
+                     labels = c("1" = "Day 1", "3" = "Day 3",
+                                "5" = "Day 5",
+                                "7" = "Day 7", "9" = "Day 9",
+                                "11" = "Day 11")) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 50)) +
-  ggtitle("Cumulative Block-Level Contacts (January)")
+  ggtitle("Cumulative Block-Level Contacts")
 
-g3 <- ggplot(rbind(cCumContactsApr, data.frame(date = as.Date("2022-04-12"), cCumContacts = tail(cCumContactsApr$cCumContacts, 1))), aes(x = date, y = cCumContacts)) +
-  geom_step() + xlab("Date") + ylab("Avg. Cumulative Cell-Level Contacts") +
-  scale_x_date(expand = c(0, 0), limits = as.Date(c("2022-04-01","2022-04-12"))) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 4)) +
-  ggtitle("Cumulative Cell-Level Contacts (April)")
-
-g4 <- ggplot(rbind(bCumContactsApr, data.frame(date = as.Date("2022-04-12"), bCumContacts = tail(bCumContactsApr$bCumContacts, 1))), aes(x = date, y = bCumContacts)) +
-  geom_step() + xlab("Date") + ylab("Avg. Cumulative Block-Level Contacts") +
-  scale_x_date(expand = c(0, 0), limits = as.Date(c("2022-04-01","2022-04-12"))) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 50)) +
-  ggtitle("Cumulative Block-Level Contacts (April)")
 
 library(patchwork)
-combined <- g1 + g2 + g3 + g4
+combined <- g1 + g2 & theme(legend.position = "right")
 combined + plot_layout(ncol = 2, guides = "collect")
-ggsave("CumulativeContacts.tiff", units="in", width=12, height = 10, dpi=300,
+ggsave("CumulativeContacts.tiff", units="in", width=12, height = 7.6, dpi=300,
        compression = 'lzw')
-ggsave("CumulativeContacts.pdf", units="in", width=12, height = 10, dpi=300)
+ggsave("CumulativeContacts.pdf", units="in", width=12, height = 7.6, dpi=300)
--- R/CumDegDists.R
+++ R/CumDegDists.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+
 # January -----------------------------------------------------------------
 
 filenames <- list.files("data/output/degDists/Jan", pattern="*.RData",
@@ -12,20 +13,9 @@ for (i in seq_len(nrow(dates))){
   cCumDist[names(cDegDists[i+1])] <- temp$cum_frequency / temp$cum_frequency[nrow(temp)] * 100
 }
 
-#Plot for 1 day in January
-# g0 <- ggplot(cCumDist, aes(x = deg, y = `2022-01-11`)) +
-#   geom_step() + xlab("Cell-Level Degree") + ylab("Cumulative %") +
-#   scale_x_continuous(expand = c(0, 0), limits = c(0, max(cCumDist$deg))) +
-#   scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
-#   ggtitle("Cumulative Cell-Level Degree Distribution (1/11)")
-
-#Plot average across January
+#Find average across January
 cCumDist$mean <- rowMeans(subset(cCumDist, select = -1))
-g1 <- ggplot(cCumDist, aes(x = deg, y = mean)) +
-  geom_step() + xlab("Cell-Level Degree") + ylab("Cumulative %") +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, max(cCumDist$deg))) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
-  ggtitle("Cumulative Cell-Level Degree Distribution (January)")
+cCumDistJan <- cCumDist[, c(1, ncol(cCumDist))]
 
 bCumDist <- data.frame(bDegDists["deg"])
 for (i in seq_len(nrow(dates))){
@@ -33,16 +23,12 @@ for (i in seq_len(nrow(dates))){
   bCumDist[names(bDegDists[i+1])] <- temp$cum_frequency / temp$cum_frequency[nrow(temp)] * 100
 }
 
-#Plot average across January
+#Find average across January
 bCumDist$mean <- rowMeans(subset(bCumDist, select = -1))
 bCumDist[nrow(bCumDist) + 1, ] <- c(50, rep(NA, nrow(dates)), 100)
-g2 <- ggplot(bCumDist, aes(x = deg, y = mean)) +
-  geom_step() + xlab("Block-Level Degree") + ylab("Cumulative %") +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, max(bCumDist$deg))) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
-  ggtitle("Cumulative Block-Level Degree Distribution (January)")
+bCumDistJan <- bCumDist[, c(1, ncol(bCumDist))]
 
-rm(list=setdiff(ls(), c("g1", "g2")))
+rm(list=setdiff(ls(), c("bCumDistJan", "cCumDistJan")))
 
 
 # April -------------------------------------------------------------------
@@ -57,13 +43,9 @@ for (i in seq_len(nrow(dates))){
   cCumDist[names(cDegDists[i+1])] <- temp$cum_frequency / temp$cum_frequency[nrow(temp)] * 100
 }
 
-#Plot average across April
+#Find average across April
 cCumDist$mean <- rowMeans(subset(cCumDist, select = -1))
-g3 <- ggplot(cCumDist, aes(x = deg, y = mean)) +
-  geom_step() + xlab("Cell-Level Degree") + ylab("Cumulative %") +
-  scale_x_continuous(expand = c(0, 0), limits = c(0, max(cCumDist$deg))) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
-  ggtitle("Cumulative Cell-Level Degree Distribution (April)")
+cCumDistApr <- cCumDist[, c(1, ncol(cCumDist))]
 
 bCumDist <- data.frame(bDegDists["deg"])
 for (i in seq_len(nrow(dates))){
@@ -71,20 +53,49 @@ for (i in seq_len(nrow(dates))){
   bCumDist[names(bDegDists[i+1])] <- temp$cum_frequency / temp$cum_frequency[nrow(temp)] * 100
 }
 
-#Plot average across April
+#Find average across April
 bCumDist$mean <- rowMeans(subset(bCumDist, select = -1))
-g4 <- ggplot(bCumDist, aes(x = deg, y = mean)) +
-  geom_step() + xlab("Block-Level Degree") + ylab("Cumulative %") +
+bCumDistApr <- bCumDist[, c(1, ncol(bCumDist))]
+
+rm(list=setdiff(ls(), c("bCumDistJan", "cCumDistJan", "bCumDistApr", "cCumDistApr")))
+
+
+# Plot --------------------------------------------------------------------
+
+library(zoo)
+library(reshape2)
+cCumDist <- merge(cCumDistJan, cCumDistApr, by = "deg", all = TRUE)
+cCumDist$mean.x <- na.locf(cCumDist$mean.x)
+cCumDist <- rename(cCumDist, Jan = mean.x)
+cCumDist <- rename(cCumDist, Apr = mean.y)
+cCumDist <- melt(cCumDist, id='deg')
+cCumDist <- rename(cCumDist, Month = variable)
+
+g1 <- ggplot() + geom_step(data = cCumDist, aes(x = deg, y = value, color = Month, group = Month, alpha = Month), size = 1) +
+  xlab("Cell-Level Degree") + ylab("Cumulative %") +
+  scale_color_manual(values=c("darkred", "cornflowerblue")) + scale_alpha_manual(values = c(1, 0.8)) +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, max(cCumDist$deg))) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
+  ggtitle("Cumulative Cell-Level Degree Distribution")
+
+bCumDist <- merge(bCumDistJan, bCumDistApr, by = "deg", all = TRUE)
+bCumDist$mean.x[1:7] <- 0
+bCumDist$mean.x <- na.locf(bCumDist$mean.x)
+bCumDist <- rename(bCumDist, Jan = mean.x)
+bCumDist <- rename(bCumDist, Apr = mean.y)
+bCumDist <- melt(bCumDist, id='deg')
+bCumDist <- rename(bCumDist, Month = variable)
+
+g2 <- ggplot() + geom_step(data = bCumDist, aes(x = deg, y = value, color = Month, group = Month, alpha = Month), size = 1) +
+  xlab("Block-Level Degree") + ylab("Cumulative %") +
+  scale_color_manual(values=c("darkred", "cornflowerblue")) + scale_alpha_manual(values = c(1, 0.8)) +
   scale_x_continuous(expand = c(0, 0), limits = c(0, max(bCumDist$deg))) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 105)) +
-  ggtitle("Cumulative Block-Level Degree Distribution (April)")
-
-
-rm(list=setdiff(ls(), c("g1", "g2", "g3", "g4")))
+  ggtitle("Cumulative Block-Level Degree Distribution")
 
 library(patchwork)
-combined <- g1 + g2 + g3 + g4
+combined <- g1 + g2 & theme(legend.position = "right")
 combined + plot_layout(ncol = 2, guides = "collect")
-ggsave("CumulativeDegDist.tiff", units="in", width=12, height = 10, dpi=300,
+ggsave("CumulativeDegDist.tiff", units="in", width=12, height = 7.6, dpi=300,
        compression = 'lzw')
-ggsave("CumulativeDegDist.pdf", units="in", width=12, height = 10, dpi=300)
+ggsave("CumulativeDegDist.pdf", units="in", width=12, height = 7.6, dpi=300)
